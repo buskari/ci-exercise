e340d6446822bdb86b9247bd74493ec54b9b2865
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-10-11 14:50:13 -0300

    feat: modified test to not pass

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-    total := Sum(17, 17)
+    total := Sum(15, 17)
 
     if total != 34 {
         t.Errorf("\nExpected: %d\nGot: %d", 34, total)
